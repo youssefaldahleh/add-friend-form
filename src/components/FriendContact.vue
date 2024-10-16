8696dc065c09bd9d58bd3efe664817f09f21fc4d
youssef <yousef.aldahleh@gmial.com>
2024-10-16 14:38:38 +0300

    first commit

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,47 @@
+<template>
+  <li>
+    <h2>{{ name }} {{isFavorite  ? "(Fav)" : "" }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <button @click="toggleFavrite">Favorite</button>
+    <button @click="Delete">Delete</button>
+    
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["id","name", "phoneNumber", "emailAddress", "isFavorite"],
+  data() {
+    return {
+      detailsAreVisible: false,
+   
+      
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavrite(){
+      this.$emit('tog-fav',this.id)
+    },
+    Delete(){
+      this.$emit('del-friend',this.id)
+    }
+
+
+  },
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,38 @@
+<template>
+  <form action="" @submit.prevent>
+    <div class="">
+      <label for="">Name</label>
+      <input type="text" v-model="enteredname"/>
+    </div>
+    <div class="">
+      <label for="">Phone</label>
+      <input type="text" v-model="enteredphone"/>
+    </div>
+    <div class="">
+      <label for="">E-mail</label>
+      <input type="text" v-model="enteredemail"/>
+    </div>
+    <button @click="addcontact">Add Friend</button>
+  </form>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      enteredname: "",
+      enteredphone: "",
+      enteredemail: "",
+    };
+  },
+  methods: {
+    addcontact() {
+      this.$emit(
+        "add-contact",
+        this.enteredname,
+        this.enteredphone,
+        this.enteredemail
+      );
+    },
+  },
+};
+</script>
